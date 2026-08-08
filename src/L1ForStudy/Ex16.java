bb996421fb785c3002c56470247ccfff28262b7c
Ana Clara <aninhaandrade888@gmail.com>
2026-08-08 02:41:39 -0300

    Exercicio 16: exibir numeros de 1 a 10 com for

---
+++ src/L1ForStudy/Ex16.java
@@ -0,0 +1,12 @@
+package L1ForStudy;
+
+//Exiba os números de 1 a 10 na tela.
+
+public class Ex16 {
+    public static void main(String[] svg) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("| %d |", i + 1);
+        }
+    }
+}
