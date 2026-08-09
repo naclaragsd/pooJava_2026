b612998d10f50cead89671c5634ccda8cc12d1ca
Ana Clara <aninhaandrade888@gmail.com>
2026-08-09 01:19:07 -0300

    Exercicio 21: soma de 1 a 100 com for e acumulador

---
+++ src/L1ForStudy/Ex21.java
@@ -0,0 +1,17 @@
+package L1ForStudy;
+
+// Some todos os números de 1 a 100 e exiba o resultado.
+
+public class Ex21 {
+    public static void main(String[] svg){
+
+        int soma = 0;
+
+        for(int i = 1; i <= 100; i++){
+            soma = soma + i;
+
+        }
+
+        System.out.printf("%d",soma);
+    }
+}
