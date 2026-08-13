24da4febe4a627ec359d067b66ef82a4ab83b8d7
Ana Clara <aninhaandrade888@gmail.com>
2026-08-13 11:14:12 -0300

    Diagrama de classes da Carro (Lista 2)

--- src/L2ForStudy/Carro.java
+++ src/L2ForStudy/Carro.java
@@ -4,4 +4,15 @@ public class Carro {
     String marca;
     String modelo;
     int ano;
+
+    Carro(String marca, String modelo, int ano){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano =ano;
+    }
+
+    void exibirDados(){
+        System.out.printf("\nMarca: %s \nModelo: %s \nAno: %d \n",
+                marca, modelo, ano);
+    }
 }
