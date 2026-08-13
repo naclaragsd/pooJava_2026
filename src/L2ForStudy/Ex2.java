533feb7b517df193cdf8d4325f13fca51013bd16
Ana Clara <aninhaandrade888@gmail.com>
2026-08-13 11:13:05 -0300

    Ex2: adiciona metodo exibirDados() na classe Carro'

---
+++ src/L2ForStudy/Ex2.java
@@ -0,0 +1,8 @@
+package L2ForStudy;
+
+public class Ex2 {
+    public static void main(String[] args){
+        Carro ferrari = new Carro("ferrari","novo",2026);
+        ferrari.exibirDados();
+    }
+}
